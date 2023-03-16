ce7b37553f0e366d68800b4dadcb73249d36de90
MazaArt <84805964+MazaArt@users.noreply.github.com>
2023-03-16 09:36:22 -0400

    Submission Lab 5

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 const express = require( "express" );
 const logger = require("morgan");
 const db = require('./db/db_connection');
+const { auth } = require('express-openid-connect');
 const app = express();
 const port = 8080;
+const dotenv = require('dotenv');
+dotenv.config();
 app.set( "views",  __dirname + "/views");
 app.set( "view engine", "ejs" );
 
@@ -16,10 +19,37 @@ app.use( express.urlencoded({ extended: false }) );
 // define middleware that serves static resources in the public directory
 app.use(express.static(__dirname + '/public'));
 
+app.use((req, res, next) => {
+    res.locals.isLoggedIn = req.oidc.isAuthenticated();
+    res.locals.user = req.oidc.user;
+    next();
+})
+
 // define a route for the funny starting page (PayWall)
 app.get( "/PayWall", ( req, res ) => {
     res.render("PayWall", data);
 } );
+
+// app.use(helmet({...}));
+
+app.use(auth(config));
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
+
+app.get('/authtest', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
+
+  const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.AUTH0_SECRET,
+    baseURL: process.env.AUTH0_BASE_URL,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+  };
 
 const delete_item_sql = `
     DELETE 
@@ -27,9 +57,11 @@ const delete_item_sql = `
         Questions
     WHERE
         id = ?
+    AND 
+        creator = ?
 `
-app.get("/list/practice/:id/delete", ( req, res ) => {
-    db.execute(delete_item_sql, [req.params.id], (error, results) => {
+app.get("/list/practice/:id/delete", requiresAuth(), ( req, res ) => {
+    db.execute(delete_item_sql, [req.params.id, req.oidc.user.email], (error, results) => {
         if (error)
             res.status(500).send(error); //Internal Server Error
         else {
@@ -48,10 +80,12 @@ const read_list_all_sql = `
         id, question, answer, creator
     FROM
         Questions
+    WHERE 
+        creator = ?
 `
 
-app.get( "/list", ( req, res ) => {
-    db.execute(read_list_all_sql, (error, results) => {
+app.get( "/list", requiresAuth(), ( req, res ) => {
+    db.execute(read_list_all_sql, [req.oidc.user.email], (error, results) => {
         if (error)
             res.status(500).send(error); //Internal Server Error
         else {
@@ -66,8 +100,8 @@ const create_item_sql = `
     VALUES
         (?, ?, ?)
 `
-app.post("/list", ( req, res ) => {
-    db.execute(create_item_sql, [req.body.name, req.body.quantity], (error, results) => {
+app.post("/list", requiresAuth(), ( req, res ) => {
+    db.execute(create_item_sql, [req.body.name, req.body.quantity, req.oidc.user.email], (error, results) => {
         if (error)
             res.status(500).send(error); //Internal Server Error
         else {
@@ -86,9 +120,11 @@ const update_item_sql = `
         creator = ?
     WHERE
         id = ?
+    AND
+        creator = ?
 `
 app.post("/list/practice/:id", ( req, res ) => {
-    db.execute(update_item_sql, [req.body.name, req.body.quantity, req.body.description, req.params.id], (error, results) => {
+    db.execute(update_item_sql, [req.body.name, req.body.quantity, req.body.description, req.params.id, req.oidc.user.email], (error, results) => {
         if (error)
             res.status(500).send(error); //Internal Server Error
         else {
@@ -110,9 +146,11 @@ const read_item_sql = `
         Questions
     WHERE
         id = ?
+    AND 
+        creator = ?
 `
-app.get( "/list/practice/:id", ( req, res, next ) => {
-    db.execute(read_item_sql, [req.params.id], (error, results) => {
+app.get( "/list/practice/:id", requiresAuth(), ( req, res, next ) => {
+    db.execute(read_item_sql, [req.params.id, req.oidc.user.email], (error, results) => {
         if (error)
             res.status(500).send(error); //Internal Server Error
         else if (results.length == 0)
@@ -126,7 +164,7 @@ app.get( "/list/practice/:id", ( req, res, next ) => {
 
 
 // define a route for the payment page
-app.get( "/ask", ( req, res ) => {
+app.get( "/ask", requiresAuth(), ( req, res ) => {
     res.render("AskYourOwn", data);
 } );
 
